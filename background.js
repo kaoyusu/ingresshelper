cb2b8678f8acd5e5d5dd947fbe6778b29a802b58
Kaoyu Su <kaoyu.su@gmail.com>
2015-07-09 23:07:32 +0800

    汉化并加入纠偏选项

--- background.js
+++ background.js
@@ -6,10 +6,22 @@
         inProgress = false,
         taskManager = new TaskManager(),
 	    helpResponse = [
-            'Send your location to the bot, then select map zoom (3 - 17). Happy Ingressing!',
-            'Authors: @veikus and @fivepointseven',
-            'Source code: http://github.com/veikus/ingresshelper'
+            '发送位置信息到当前机器人, 然后选择是否纠偏及地图缩放等级(3 - 17). Happy Ingressing!',
+            '图片压缩启用及关闭请使用 /compression_on 和 /compression_off',
+            'Authors: @veikus and @fivepointseven, Modified @kaoyu',
+            'Source code: https://github.com/kaoyusu/ingresshelper'
         ],
+	    // Custom keyboard markup:
+	    rectifyMarkup = {
+            keyboard: [
+                ['true'],
+                ['17 - All portals'],
+                ['16', '15', '14', '13'],
+                ['12', '10', '8', '6'],
+                ['3 - World']
+            ],
+            one_time_keyboard: true
+        },
 	    // Custom keyboard markup:
 	    levelMarkup = {
             keyboard: [
@@ -99,7 +111,7 @@
 
         if (task.message.location) {
             // Ask for zoom and cache location request
-            sendResponse(task, 'Select zoom level', levelMarkup);
+            sendResponse(task, '由于Telegram内地图偏移,需纠偏请选true,否则直接选择缩放等级', rectifyMarkup);
             taskManager.addTask(task);
         } else {
             switch (task.message.text) {
@@ -121,7 +133,7 @@
                     chatSettings[chatId].noCompression = false;
                     localStorage.setItem('chatSettings', JSON.stringify(chatSettings));
 
-                    sendResponse(task, 'Compression enabled');
+                    sendResponse(task, '图片压缩已启用');
                     break;
 
                 case '/compression_off':
@@ -131,26 +143,34 @@
                     chatSettings[chatId].noCompression = true;
                     localStorage.setItem('chatSettings', JSON.stringify(chatSettings));
 
-                    sendResponse(task, 'Compression disabled');
+                    sendResponse(task, '图片压缩已取消');
+                    break;
+
+                case 'true':
+                    if(taskManager.setRectify(task)){
+                        sendResponse(task, '选择缩放等级', levelMarkup);
+                    }else{
+                        sendResponse(task, '请先发送位置信息');
+                    }
                     break;
 
                 default:
                     if (allowedLevelOptions.indexOf(task.message.text) > -1) {
                         z = parseInt(task.message.text);
                     } else if (!isGroup) {
-                        sendResponse(task, 'Incorrect command.');
+                        sendResponse(task, '错误命令.');
                     }
             }
 
             if (z) {
                 if (taskManager.setZoom(task, z)) {
-                    sendResponse(task, 'Task created. Please wait less than ' + taskManager.calculateEstimateTime());
+                    sendResponse(task, '任务已建立. 请等待大约 ' + taskManager.calculateEstimateTime());
 
                     if (!inProgress) {
                         startNextTask();
                     }
                 } else {
-                    sendResponse(task, 'Please send location first');
+                    sendResponse(task, '请先发送位置信息');
                 }
             }
 
@@ -208,6 +228,12 @@
         inProgress = task;
         latitude = task.message.location.latitude;
         longitude = task.message.location.longitude;
+
+        if(task.rectify){
+	    var ret = gcj2wgs(latitude, longitude);
+	    latitude = ret.lat.toFixed(6).toString();
+	    longitude = ret.lng.toFixed(6).toString();
+        }
 
 	    // Set higher timeout for L7+ portals
 	    if (task.zoom <= 7) {
@@ -292,6 +318,18 @@
             incompleteQueue[from] = task;
         };
 
+        this.setRectify = function(task) {
+            var from = task.message.from.id;
+
+            if (!incompleteQueue[from]) {
+                return false;
+            }
+
+            incompleteQueue[from].rectify = true;
+
+            return true;
+        };
+
         /**
          * Set zoom level for users incomplete task
          * @param task {object} Telegram object from getUpdates
@@ -330,10 +368,10 @@
             }
 
             if (est < 60) {
-                return est + ' seconds';
+                return est + ' 秒';
             } else {
                 left = Math.ceil(est / 60);
-                return left + ' minute' + (left > 1 ? 's' : '');
+                return left + ' 分钟';
             }
         };
 
@@ -345,5 +383,55 @@
             return queue.shift();
         }
     }
+
+//https://github.com/googollee/eviltransform/blob/master/javascript/transform.js
+
+function outOfChina(lat, lng) {
+	if ((lng < 72.004) || (lng > 137.8347)) {
+		return true;
+	}
+	if ((lat < 0.8293) || (lat > 55.8271)) {
+		return true;
+	}
+	return false;
+}
+
+function transformLat(x, y) {
+	var ret = -100.0 + 2.0*x + 3.0*y + 0.2*y*y + 0.1*x*y + 0.2*Math.sqrt(Math.abs(x));
+	ret += (20.0*Math.sin(6.0*x*Math.PI) + 20.0*Math.sin(2.0*x*Math.PI)) * 2.0 / 3.0;
+	ret += (20.0*Math.sin(y*Math.PI) + 40.0*Math.sin(y/3.0*Math.PI)) * 2.0 / 3.0;
+	ret += (160.0*Math.sin(y/12.0*Math.PI) + 320*Math.sin(y*Math.PI/30.0)) * 2.0 / 3.0;
+	return ret;
+}
+
+function transformLon(x, y) {
+	var ret = 300.0 + x + 2.0*y + 0.1*x*x + 0.1*x*y + 0.1*Math.sqrt(Math.abs(x));
+	ret += (20.0*Math.sin(6.0*x*Math.PI) + 20.0*Math.sin(2.0*x*Math.PI)) * 2.0 / 3.0;
+	ret += (20.0*Math.sin(x*Math.PI) + 40.0*Math.sin(x/3.0*Math.PI)) * 2.0 / 3.0;
+	ret += (150.0*Math.sin(x/12.0*Math.PI) + 300.0*Math.sin(x/30.0*Math.PI)) * 2.0 / 3.0;
+	return ret;
+}
+
+function delta(lat, lng) {
+	var a = 6378245.0;
+	var ee = 0.00669342162296594323;
+	var dLat = transformLat(lng-105.0, lat-35.0);
+	var dLng = transformLon(lng-105.0, lat-35.0);
+	var radLat = lat / 180.0 * Math.PI;
+	var magic = Math.sin(radLat);
+	magic = 1 - ee*magic*magic;
+	var sqrtMagic = Math.sqrt(magic);
+	dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * Math.PI);
+	dLng = (dLng * 180.0) / (a / sqrtMagic * Math.cos(radLat) * Math.PI);
+	return {"lat": dLat, "lng": dLng};
+}
+
+function gcj2wgs(gcjLat, gcjLng) {
+	if (outOfChina(gcjLat, gcjLng)) {
+		return {"lat": gcjLat, "lng": gcjLng};
+	}
+	var d = delta(gcjLat, gcjLng);
+	return {"lat": gcjLat - d.lat, "lng": gcjLng - d.lng};
+}
 
 }());
